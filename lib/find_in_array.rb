003f12ae15059c698b71eea9d76715372c1cda2b
Jodi Croft <jodipruett@gmail.com>
2020-05-18 20:04:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0
+  while value_to_find != array[i] do
+    i++
+  end
 end
+
+
+
+
+Finding data find_element_index(array, value_to_find) takes in an array and a value and returns the index of that value
+     Failure/Error: expect(find_element_index(scale,2)).to eq(0)
